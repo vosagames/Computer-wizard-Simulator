fc31d2054e3f5d6ef08fb25f1cf3732bad4c0366
vosagames <alexsandrvo0405@mail.ru>
2024-11-19 23:56:19 +0400

    Update 0.3
    add Computer Script, moving computers  in level

--- Assets/Scripts/PlayerInteractiveRay.cs
+++ Assets/Scripts/PlayerInteractiveRay.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerInteractiveRay : MonoBehaviour
 {
+    [SerializeField] private Transform grabPosition;
+    [SerializeField] private GameObject Computer;
+
+    private bool isGrab;
+
     private void Update()
     {
         Ray ray = new Ray(transform.position, transform.forward);
@@ -11,7 +16,23 @@ public class PlayerInteractiveRay : MonoBehaviour
         Debug.DrawRay(transform.position, transform.forward, Color.blue, 5f);
         if (Physics.Raycast(ray, out hit, 5f))
         {
-            
+            if(isGrab == false && hit.collider.GetComponent<ComputerSettings>() && Input.GetKeyDown(KeyCode.E))
+            {
+                isGrab = true;
+                Computer = hit.collider.GetComponent<ComputerSettings>().gameObject;
+                Computer.GetComponent<BoxCollider>().enabled = false;
+                Computer.transform.position = grabPosition.position;
+                Computer.transform.SetParent(grabPosition);
+            }
+            if(isGrab == true && hit.collider.CompareTag("RepairTable") && Input.GetKeyDown(KeyCode.E))
+            {
+                GameObject repairTable = hit.collider.gameObject;
+                Computer.GetComponent<BoxCollider>().enabled = true;
+                Computer.transform.position = repairTable.transform.GetChild(3).position;
+                Computer.transform.SetParent(repairTable.transform.GetChild(3));
+                Computer = null;
+                isGrab = false;
+            }
         }
     }
 }
